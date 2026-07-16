6ec7a93cb341637d9db0f8b6634799f64a3af6f2
Michael Greatorex <michael.greatorex@leonardocompany.com>
2026-07-16 16:41:53 +0100

    connect ASP-NET to PostgreSQL

---
+++ Api/Models/HealthCheckRecord.cs
@@ -0,0 +1,12 @@
+﻿namespace Api.Models;
+
+public class HealthCheckRecord
+{
+    public int Id { get; set; }
+
+    public string Status { get; set; } = string.Empty;
+
+    public int ResponseTimeMs { get; set; }
+
+    public DateTime CheckedAt { get; set; }
+}
